802703143135f2ae1a61abbc7c87a9e506ac69c0
bukolastella <abolarinbukolastella@gmail.com>
2022-05-19 04:38:35 +0100

    added new changes

--- Front-end/components/main page/MainPage.js
+++ Front-end/components/main page/MainPage.js
@@ -10,22 +10,17 @@ import img6 from "../../public/6.svg";
 import img7 from "../../public/7.svg";
 import img8 from "../../public/8.svg";
 import img9 from "../../public/9.svg";
+import img10 from "../../public/10.svg";
 
 const MainPage = () => {
-  const renderCards = () => {
-    let arr = [];
-    for (let index = 1; index < 9; index++) {
-      arr.push(<Cards imgName={index} />);
-    }
-    return arr;
-  };
-
   return (
     <div className="p-20 bg-[rgba(211,219,206)]">
       <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3  md:grid-cols-4 gap-10">
-        {[img1, img2, img3, img4, img5, img6, img7, img8, img9].map((ev, i) => (
-          <Cards imgName={ev} key={i} tokenId={i + 1} />
-        ))}
+        {[img1, img2, img3, img4, img5, img6, img7, img8, img9, img10].map(
+          (ev, i) => (
+            <Cards imgName={ev} key={i} tokenId={i + 1} />
+          )
+        )}
       </div>
     </div>
   );
